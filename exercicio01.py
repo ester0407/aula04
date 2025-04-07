e65c6f12dc411dd85f9dc70d6dda5d367105ec10
ester araujo <esteraraujosantos2006@gmail.com>
2025-04-07 09:47:08 -0300

    enviando exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,30 @@
+numero= int(input("digite um numeero"))
+if numero>12:
+    print("numero invalido")
+elif numero==1:
+    print(f"numero=janeiro")
+elif numero==2:
+    print(f"numero=fevereiro")
+elif numero==3:
+    print(f"numero=março")
+elif numero==4:
+    print(f"numero=abril")
+elif numero==5:
+    print("numero=maio")
+elif numero==6:
+    print("numero=junho")
+elif numero==7:
+    print("numero=julho")
+elif numero==8:
+    print("numero=agosto")
+elif numero==9:
+    print("numero=setembro")
+elif numero==10:
+    print("numero=outubro")
+elif numero==11:
+    print("numero=novembro")
+else:
+    print("numero=dezembro")
+
+
+
